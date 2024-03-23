fa5d71cae3311c59b9c8308edaf9d2a9d07b1630
zuiyu1998 <1542844298@qq.com>
2024-03-23 20:39:32 +0800

    refactor: 去除无效代码

--- src/audio.rs
+++ src/audio.rs
@@ -1,5 +1,3 @@
-use crate::loading::AudioAssets;
-use crate::GameState;
 use bevy::prelude::*;
 use bevy_kira_audio::prelude::*;
 
@@ -8,20 +6,6 @@ pub struct InternalAudioPlugin;
 // This plugin is responsible to control the game audio
 impl Plugin for InternalAudioPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(AudioPlugin)
-            .add_systems(OnEnter(GameState::Playing), start_audio);
+        app.add_plugins(AudioPlugin);
     }
-}
-
-#[derive(Resource)]
-struct FlyingAudio(Handle<AudioInstance>);
-
-fn start_audio(mut commands: Commands, audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
-    audio.pause();
-    let handle = audio
-        .play(audio_assets.flying.clone())
-        .looped()
-        .with_volume(0.3)
-        .handle();
-    commands.insert_resource(FlyingAudio(handle));
 }
